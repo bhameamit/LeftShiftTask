a316ef97301548e7c7dea8250e703224eedef6b8
unknown <bhame.amit@gmail.com>
2015-02-23 23:25:20 +0530

    added basic code

---
+++ LeftShiftTask/LeftShiftTak/src/com/example/net/ProxyInterface.java
@@ -0,0 +1,9 @@
+package com.example.net;
+
+public interface ProxyInterface {
+	
+	
+	//public void responseFromService(String response);
+	public void responseFromService(String response, String url);
+
+}
